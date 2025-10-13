fe36d818b94dd07a115c94243389acd3caf27503
alexakode <alexanderm.kodehode@gmail.com>
2025-10-13 14:07:22 +0200

    testing calculate

--- Serveren/index.js
+++ Serveren/index.js
@@ -1,3 +1,8 @@
 const os = require("os");
 const path = require("path");
+const calculate = require("./calculate");
 console.log(path.parse(__filename));
+console.log(calculate.add(5, 3));
+console.log(calculate.subtract(5, 3));
+console.log(calculate.multiply(5, 3));
+console.log(calculate.divide(5, 3));
